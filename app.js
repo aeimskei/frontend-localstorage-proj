821dd3d241f950dbe4069e39ce70360b365cc80e
Amy Boudsady <aeimskei@gmail.com>
2018-03-06 07:01:57 -0800

    Show name inputs into a list below in the user-interface

--- app.js
+++ app.js
@@ -8,13 +8,32 @@ function supportsLocalStorage() {
   }
 }
 
-// Retrieve names from Local Storage, return an array
+// Get names from Local Storage, return an array
 function getRecentNames() {
   let names = localStorage.getItem('recentNames');
   if (names) {
     return JSON.parse(names);
   }
   return [];
+}
+
+// Validate and save names to store of past names
+function saveinputNames(name) {
+  let names = getRecentNames();
+  if (names.indexOf(name) > -1 || !name) {
+    return false;
+  }
+  names.push(name);
+  localStorage.setItem('recentNames', JSON.stringify(names));
+  return true;
+}
+
+// Helper function
+// Create an li, given name input, append to the supplied ul
+function appendListItem(listElement, name) {
+  let listItemElement = document.createElement('LI');
+  listItemElement.innerHTML = name;
+  listElement.appendChild(listItemElement);
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
